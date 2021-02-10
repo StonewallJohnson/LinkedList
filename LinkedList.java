76806507b4754366e241646f8cf2905db1e7a6f2
Landon Johnson <landon.johnson@utexas.edu>
2021-02-09 20:30:06 -0600

    added add

--- LinkedList.java
+++ LinkedList.java
@@ -2,7 +2,42 @@ public class LinkedList<E>{
     private int size;
     Node<E> head;
 
-    private class Node<E>{
+    private class Node<T>{
+        T con;
+        Node<T> next;
+
+        public Node(T val, Node<T> link){
+            con = val;
+            next = link;
+        }
+
+        public Node(T val){
+            this(val, null);
+        }
+    }
+
+    public void add(E val){
+        checkValNull(val);
         
+        if(size == 0){
+            //adding head
+            head = new Node<E>(val);
+        }
+        else{
+            //adding other
+            Node<E> temp = head;
+            while(temp.next != null){
+                //until last node
+                temp = temp.next;
+            }
+            temp.next = new Node<E>(val);
+        }
+        size++;
+    }
+
+    private void checkValNull(E val){
+        if(val == null){
+            throw new IllegalArgumentException("Can't store null!");
+        }
     }
 }
